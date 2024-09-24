b5a23a903b26816d218a2343213be771cf9cbabd
DcViet <111166640+DcViet@users.noreply.github.com>
2024-09-24 18:37:01 +0700

    Update omd_ViewProject.tsx

--- ic-71303/src/components/omd_ViewProject.tsx
+++ ic-71303/src/components/omd_ViewProject.tsx
@@ -12,3 +12,59 @@
     <p>2024</p>
   </div>
 </div>
+
+
+<div class="visible flex h-[143px] w-[800px] flex-row justify-end gap-[20px] overflow-visible border border-black">
+  <div class="visible relative flex h-full w-[143px] flex-col items-center justify-center overflow-visible border border-black">
+    <p class="text-center text-[46px] leading-[1.3em] tracking-[0.18em]">1</p>
+  </div>
+</div>
+
+<div class="visible flex h-[1000px] w-[800px] flex-col items-start justify-center gap-[20px] overflow-visible border border-black">
+  <div class="visible relative flex h-[140px] w-[700px] flex-row items-center justify-center gap-[10px] overflow-visible border border-black">
+    <div class="justify-right visible relative flex flex-col items-start gap-[10px] overflow-visible">
+      <div>
+        <p class="text-[63px]">OMD</p>
+      </div>
+      <div>
+        <p>kiến tạo dự án</p>
+      </div>
+    </div>
+
+    <div class="justify-left visible relative flex flex-col items-end gap-[10px] overflow-visible">
+      <div>
+        <p>được đồng hành cùng chị D trong hành trình</p>
+      </div>
+      <div>
+        <p class="text-[63px]">LAPH cafe</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="visible relative flex flex-col items-center justify-center overflow-visible border border-black px-[10px]">
+    <!-- vertical line -->
+    <div>
+      <div>
+        <p>Nhiệm vụ thiết kế</p>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <p>“Café-Cóc” – trong tiếng Việt, thuật ngữ này chỉ quán cà phê bình dân với chỗ ngồi thấp, có thể dễ dàng gấp lại và bố trí khi cần thiết. Đó là nơi mọi người có thể ghé qua một cách nhanh chóng và thuận tiện, gọi đồ uống, nhâm nhi một mình hoặc với bạn bè, sau đó tiếp tục công việc trong ngày của mình.</p>
+  </div>
+
+  <div class="visible relative flex h-[45px] w-[700px] flex-col items-center justify-center gap-[10px] overflow-visible border border-black">
+    <!-- vertical line -->
+    <div>
+      <div>
+        <p>Bộ sưu tập</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="visible relative flex h-[370px] w-[700px] flex-col items-center justify-center gap-[10px] overflow-visible border border-black">
+    <img src="" alt="" />
+    <p>image</p>
+  </div>
+</div>
