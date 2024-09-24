9c24a30bef7a4ddc78d574d66e9462e09297f72c
DcViet <111166640+DcViet@users.noreply.github.com>
2024-09-24 11:55:39 +0700

    Update omd_ViewIntro.tsx

---
+++ ic-71303/src/components/omd_ViewIntro.tsx
@@ -0,0 +1,18 @@
+<div></div>
+
+<div class="visible flex h-[205px] w-[665px] flex-col items-center justify-start overflow-visible whitespace-nowrap pt-[10px]">
+  <p class="relative overflow-visible text-[115px] leading-[1em] tracking-[0.11em]">OMD</p>
+  <p class="relative overflow-visible text-center text-[18px] leading-[1.4em] tracking-[0.04em]">Văn phòng thiết kế kiến trúc - nội thất</p>
+
+  <div class="visible flex h-[347px] w-[710px] flex-col items-center justify-center gap-[40px] overflow-visible whitespace-nowrap p-[40px]">
+    <p class="relative overflow-visible whitespace-normal text-center text-[18px] leading-[1.4em] tracking-[0.04em]">Không gian là của bạn, của người thân, của gia đình bạn. Chúng tôi khởi đầu mỗi dự án bằng việc lắng nghe, thấu hiểu nhu cầu và mong muốn của bạn. Với OMD Architect, kiến trúc thực sự đẹp là khi không gian trong đó có thể nuôi dưỡng tâm hồn, mang lại sự thư thái và khơi dậy cảm hứng.</p>
+
+    <div class="visible flex flex-col items-center justify-center overflow-visible">
+      <button class="text-center text-[18px] leading-[1.4em] tracking-[0.04em]">Kết nối với chúng tôi</button>
+    </div>
+  </div>
+</div>
+
+<div>
+  <!-- component -->
+</div>
