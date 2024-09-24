d49e05386b2456b13e411622819b51db2cd3deb8
DcViet <111166640+DcViet@users.noreply.github.com>
2024-09-24 11:23:44 +0700

    Update omd_ViewBlog.tsx

---
+++ ic-71303/src/components/omd_ViewBlog.tsx
@@ -0,0 +1,18 @@
+
+<div class="flex h-[34px] w-[832px] flex-row items-start justify-center gap-[48px] overflow-visible whitespace-nowrap">
+  <div class="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
+    <a href="#">Service</a>
+  </div>
+  <div class="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
+    <a href="#">Project</a>
+  </div>
+  <div class="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
+    <a href="#">OMD ..?</a>
+  </div>
+  <div class="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
+    <a href="#">About</a>
+  </div>
+  <div class="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
+    <a href="#">Blog</a>
+  </div>
+</div>
