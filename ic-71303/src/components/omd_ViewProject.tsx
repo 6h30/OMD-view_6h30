0add21040d451d5c7b92b48b42f6752c5dcec5d4
DcViet <111166640+DcViet@users.noreply.github.com>
2024-09-24 16:26:48 +0700

    Update omd_ViewProject.tsx

---
+++ ic-71303/src/components/omd_ViewProject.tsx
@@ -0,0 +1,14 @@
+
+<div class="visible absolute flex h-[60px] w-[700px] flex-row items-center justify-center gap-[10px] overflow-visible border border-black">
+  <div class="visible relative flex h-[40px] w-[169px] flex-row items-start justify-center gap-[10px] overflow-visible pl-[10px]">
+    <p>Diện tích:</p>
+    <p>208m2</p>
+  </div>
+  <div class="visible relative flex h-[40px] w-[318px] flex-row items-start justify-right gap-[10px] overflow-visible border-l border-black pl-[10px]">
+    <p>Kts:</p>
+  </div>
+  <div class="visible relative flex h-[40px] w-[169px] flex-row items-start justify-center gap-[10px] overflow-visible border-l border-black pl-[10px]">
+    <p>Năm</p>
+    <p>2024</p>
+  </div>
+</div>
