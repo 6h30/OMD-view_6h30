7a87ab1bd642f995b1714f0d7fb02f2cc3d58593
DcViet <111166640+DcViet@users.noreply.github.com>
2024-09-24 11:24:31 +0700

    Update omd_ViewBlog.tsx

--- ic-71303/src/components/omd_ViewBlog.tsx
+++ ic-71303/src/components/omd_ViewBlog.tsx
@@ -1,18 +1,26 @@
+// src/components/omd_NameFrame.tsx
+import Image from "next/image";
+// import styles from './omd_NameFrame.module.css';
 
-<div class="flex h-[34px] w-[832px] flex-row items-start justify-center gap-[48px] overflow-visible whitespace-nowrap">
-  <div class="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
+export default function NameFrame() {
+  return (
+    
+<div className="flex h-[34px] w-[832px] flex-row items-start justify-center gap-[48px] overflow-visible whitespace-nowrap">
+  <div className="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
     <a href="#">Service</a>
   </div>
-  <div class="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
+  <div className="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
     <a href="#">Project</a>
   </div>
-  <div class="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
+  <div className="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
     <a href="#">OMD ..?</a>
   </div>
-  <div class="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
+  <div className="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
     <a href="#">About</a>
   </div>
-  <div class="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
+  <div className="visible flex h-[32px] w-[132px] flex-row items-center justify-center gap-[10px] overflow-visible rounded-[5px] border border-black">
     <a href="#">Blog</a>
   </div>
 </div>
+);
+}
