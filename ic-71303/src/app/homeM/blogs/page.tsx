25f180726803cae252c344e94cd39f3d74e793aa
DcViet <tranducviet.eng@gmail.com>
2024-11-10 18:16:04 +0700

    responsive blog

--- ic-71303/src/app/homeM/blogs/page.tsx
+++ ic-71303/src/app/homeM/blogs/page.tsx
@@ -5,7 +5,7 @@ import ViewBlogs from '@/components/omd_ViewBlogs';
 export default function Blogs() {
   return (
     <div className="flex h-full w-full items-start justify-center overflow-y-auto scroll-smooth border">
-      <div className="font-koho w-[1024px]">
+      <div className="font-koho md:w-[820px] sm:w-[420px] h-[800px]">
         <ViewBlogs />
       </div>
     </div>
